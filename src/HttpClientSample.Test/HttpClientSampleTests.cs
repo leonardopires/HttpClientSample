9e60d7ed51b0cfda8eb6f5915b55a3706e736efe
Leonardo Thome Pires <leonardo.pires@aurea.com>
2017-08-30 15:38:46 -0300

    Added test to check whether HttpClient is always the same

---
+++ src/HttpClientSample.Test/HttpClientSampleTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using Autofac;
+using FluentAssertions;
+using HttpClientSample.Core;
+using Xunit;
+
+namespace HttpClientSample.Test
+{
+    public class HttpClientSampleTests
+    {
+        [Fact]
+        public void ContainerShouldAlwaysReturnTheSameHttpClient()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<CoreModule>();
+            IContainer container = builder.Build();
+
+            var client1 = container.Resolve<HttpClient>();
+
+            using (container.BeginLifetimeScope())
+            {
+                var client2 = container.Resolve<HttpClient>();
+
+                client2.Should().BeSameAs(client1, "There must be just one instance of HttpClient in order to keep the socket count low");
+            }
+        }
+    }
+}
